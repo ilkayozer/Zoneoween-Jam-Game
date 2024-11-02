7de01b9fd9167676b601c6977df465a785376249
Kirayorself <yusufbugra52@gmail.com>
2024-11-03 00:17:16 +0300

    prefab fixed31

--- Assets/Scripts/NpcManager.cs
+++ Assets/Scripts/NpcManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class NpcManager : MonoBehaviour
     public string npcName;
     public string[] dialogs;
     public int floor;
+
+    public bool isEvil;
 
     void Start()
     {
