3f81a98b8b80522d549c76314afea572e7ae8aaf
erkerem2 <150459729+erkerem2@users.noreply.github.com>
2024-11-02 03:47:30 +0300

    scene added

--- Assets/Scripts/DoorMovement.cs
+++ Assets/Scripts/DoorMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class DoorMovement : MonoBehaviour
 {
+
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,15 @@ public class DoorMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Close()
+    {
+        
+    }
+
+    void Open()
+    {
+
     }
 }
